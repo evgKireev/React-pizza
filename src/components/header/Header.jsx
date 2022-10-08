a672d6f0160d990eab606fc2c67afd846f253f7e
evgKireev <bob1_k@mail.ru>
2022-10-08 14:23:06 +0300

    fixed git page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
   return (
     <div className="header">
       <div className="container">
-        <Link to="/">
+        <Link to="/React-pizza">
           <div className="header__logo">
             <img width="38" src="img/pizza-logo.svg" alt="Pizza logo" />
             <div>
@@ -22,8 +22,8 @@ function Header() {
         </Link>
         <Search />
         <div className="header__cart">
-          {pathname === '/' && (
-            <Link to="/cart" className="button button--cart">
+          {pathname === '/React-pizza' && (
+            <Link to="/React-pizza/cart" className="button button--cart">
               <span>{pricePizza} ₽</span>
               <div className="button__delimiter"></div>
               <svg
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -117,7 +117,7 @@ function Cart() {
           <div className="cart__bottom-buttons">
             <Link
               className="button button--outline button--add go-back-btn"
-              to="/"
+              to="/React-pizza"
             >
               <svg
                 width="8"
